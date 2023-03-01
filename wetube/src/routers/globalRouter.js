0e5c33045cc44710db8c16e538d7fc73726c824e
TaeYoon Kim <xodbsrla1025@naver.com>
2023-03-01 20:32:49 +0900

    Update

--- wetube/src/routers/globalRouter.js
+++ wetube/src/routers/globalRouter.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { join, trending } from "../controllers/userControllers";
+import { join, login } from "../controllers/userControllers";
+import { trending, search } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 export default globalRouter;
