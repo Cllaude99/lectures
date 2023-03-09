ea90e5fd8f996c3616d29bab351ba6addf59147b
TaeYoon Kim <xodbsrla1025@naver.com>
2023-03-09 19:34:51 +0900

    Update

--- wetube/src/server.js
+++ wetube/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import routeRouter from "./routers/routeRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -12,6 +13,14 @@ app.set("views", process.cwd() + "/src/views");
 
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  }),
+);
 
 app.use("/", routeRouter);
 app.use("/videos", videoRouter);
