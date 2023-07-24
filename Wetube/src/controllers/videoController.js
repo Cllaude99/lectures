3454f45b7489b377721bc62054816a45b79b0284
TaeYoon Kim <xodbsrla1025@naver.com>
2023-07-25 00:58:05 +0900

    Ch05

--- Wetube/src/controllers/videoController.js
+++ Wetube/src/controllers/videoController.js
@@ -1,9 +1,36 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => {
-  console.log(req.params);
-  return res.send("Watch");
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const edit = (req, res) => res.send("Edit");
+export const see = (req, res) =>
+  res.render("watch", { pageTitle: "Watch Video" });
+export const edit = (req, res) =>
+  res.render("edit", { pageTitle: "Edit Video" });
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
--- Wetube/src/server.js
+++ Wetube/src/server.js
@@ -8,8 +8,10 @@ const PORT = 4000;
 const app = express();
 
 const logger = morgan("dev");
-app.use(logger);
 
+app.set("views", process.cwd() + "/src/views"); // views를 찾는 경로를 바꿔주기 위한 용도 (res.render함수에서 첫 번째 인수 값의 pug파일을 찾을때 views폴더를 찾는데 이때 경로를 재설정해줌)
+app.set("view engine", "pug"); // view engine이 pug파일임을 알려주기 위한 용도 (pug파일로 html화면을 보이겠다는 의미)
+app.use(logger);
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
