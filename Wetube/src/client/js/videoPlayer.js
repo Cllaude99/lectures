fd7acdb59d41ce11e2c3bac14e8fa14a4339ab6c
TaeYoon Kim <xodbsrla1025@naver.com>
2023-08-03 19:59:04 +0900

    Ch11 0-7

---
+++ Wetube/src/client/js/videoPlayer.js
@@ -0,0 +1,109 @@
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const volumeRange = document.getElementById("volumn");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
+const fullScreenBtn = document.getElementById("fullScreen");
+const videoContainer = document.getElementById("videoContainer");
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
+const handlePlayClick = e => {
+  // if the video is playing, pause it
+  // else play the video
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
+};
+
+const handleMuteClick = e => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeChange = event => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  if (value == 0) {
+    video.muted = true;
+    muteBtn.innerText = "Unmute";
+  }
+  volumeValue = value;
+  video.volume = value;
+};
+
+const formatTime = seconds => {
+  const startIdx = seconds >= 3600 ? 11 : 14;
+  return new Date(seconds * 1000).toISOString().substring(startIdx, 19);
+};
+
+const handleLoadedMetadata = () => {
+  totalTime.innerText = formatTime(Math.floor(video.duration));
+  timeline.max = Math.floor(video.duration);
+};
+
+const handleTimeUpdate = () => {
+  currentTime.innerText = formatTime(Math.floor(video.currentTime));
+  timeline.value = Math.floor(video.currentTime);
+};
+
+const handleTimelineChange = event => {
+  const {
+    target: { value },
+  } = event;
+  video.currentTime = value;
+};
+
+const handleKeyboard = event => {
+  if (event.keyCode == 32) {
+    handlePlayClick();
+  } else if (event.keyCode == 39) {
+    video.currentTime += 5;
+  } else if (event.keyCode == 37) {
+    video.currentTime -= 5;
+  }
+};
+
+const handleFullScreen = () => {
+  const fullscreen = document.fullscreenElement;
+  if (fullscreen) {
+    document.exitFullscreen();
+    fullScreenBtn.innerText = "Enter Full Screen";
+  } else {
+    videoContainer.requestFullscreen();
+    fullScreenBtn.innerText = "Exit Full Screen";
+  }
+};
+
+window.addEventListener("keydown", handleKeyboard);
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMuteClick);
+volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("timeupdate", handleTimeUpdate);
+video.addEventListener("click", handlePlayClick);
+timeline.addEventListener("input", handleTimelineChange);
+fullScreenBtn.addEventListener("click", handleFullScreen);
+document.onfullscreenchange = () => {
+  if (!document.fullscreenElement) {
+    fullScreenBtn.innerText = "Enter Full Screen";
+  }
+};
+video.readyState
+  ? handleMetadata()
+  : video.addEventListener("loadedmetadata", handleLoadedMetadata);
--- Wetube/webpack.config.js
+++ Wetube/webpack.config.js
@@ -1,7 +1,11 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 module.exports = {
-  entry: "./src/client/js/main.js", // 우리가 변경하고자 하는 파일 (최신 자바스크립트 문법)
+  entry: {
+    // 우리가 변경하고자 하는 파일 (최신 자바스크립트 문법)
+    main: "./src/client/js/main.js",
+    videoPlayer: "./src/client/js/videoPlayer.js",
+  },
   plugins: [
     new MiniCssExtractPlugin({
       filename: "css/styles.css",
@@ -11,7 +15,7 @@ module.exports = {
   mode: "development", // 개발모드 중임을 알리는 코드 (별도의 설정이 없으면 webpack은 mode를 default값으로 production으로 설정함. production의 경우 압축이 일어남)
   output: {
     // 처리된 결과물(모든 브라우저에서 사용가능한 문법)을 저장할 파일
-    filename: "js/main.js", // 처리된 결과물이 path에 있는 값의 폴더에 filename의 값("main.js")으로 저장된다.
+    filename: "js/[name].js", // 처리된 결과물이 path에 있는 값의 폴더에 filename의 값("main.js")으로 저장된다.(이때 name에 의해서 'assets/js/entry에 있는 이름.js'로 저장된다.)
     path: path.resolve(__dirname, "assets"), // 처리된 결과물이 저장될 폴더
     clean: true,
   },
