2a7e3d0084afad56cc578840759c9334c81476ca
TaeYoon Kim <xodbsrla1025@naver.com>
2023-08-07 17:02:41 +0900

    Fix: "thumbnail 안보이는 이슈 해결"

--- Wetube/src/controllers/videoController.js
+++ Wetube/src/controllers/videoController.js
@@ -72,7 +72,7 @@ export const postUpload = async (req, res) => {
       title,
       description,
       fileUrl: video[0].path,
-      thumbUrl: thumb[0].path,
+      thumbUrl: thumb[0].path.replace(/[\\]/g, "/"),
       owner: _id,
       hashtags: Video.formatHashtags(hashtags),
     });
