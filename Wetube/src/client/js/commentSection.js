5972f882f9cc448a43d0de77be4c1b7c4d8d46c4
TaeYoon Kim <xodbsrla1025@naver.com>
2023-08-08 18:07:51 +0900

    Ch16 7

--- Wetube/src/client/js/commentSection.js
+++ Wetube/src/client/js/commentSection.js
@@ -1,6 +1,19 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
+const addComment = (text) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.classList = "video__comment";
+  const icon = document.createElement("i");
+  icon.classList = "fas fa-comment";
+  const span = document.createElement("span");
+  span.innerText = ` ${text}`;
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  videoComments.prepend(newComment);
+};
+
 const handleSubmit = async (event) => {
   event.preventDefault(); // form에 제출할때 기본적으로 새로고침이 일어나는데 이를 방지하는 역할을 해줌.
   const textarea = form.querySelector("textarea");
@@ -10,7 +23,7 @@ const handleSubmit = async (event) => {
   if (text === "" || text.trim() === "") {
     return;
   }
-  await fetch(`/api/videos/${videoId}/comment`, {
+  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -18,6 +31,9 @@ const handleSubmit = async (event) => {
     body: JSON.stringify({ text }),
   });
   textarea.value = "";
+  if (status === 201) {
+    addComment(text);
+  }
 };
 
 if (form) {
