2567cb877e2eced986ab927ae1c820df491273e7
TaeYoon Kim <xodbsrla1025@naver.com>
2023-08-09 19:11:23 +0900

    Ch17 1-2

--- Wetube/webpack.config.js
+++ Wetube/webpack.config.js
@@ -16,8 +16,7 @@ module.exports = {
       filename: "css/styles.css",
     }),
   ],
-  watch: true,
-  mode: "development", // 개발모드 중임을 알리는 코드 (별도의 설정이 없으면 webpack은 mode를 default값으로 production으로 설정함. production의 경우 압축이 일어남)
+  // 개발모드 중임을 알리는 코드 (별도의 설정이 없으면 webpack은 mode를 default값으로 production으로 설정함. production의 경우 압축이 일어남)
   output: {
     // 처리된 결과물(모든 브라우저에서 사용가능한 문법)을 저장할 파일
     filename: "js/[name].js", // 처리된 결과물이 path에 있는 값의 폴더에 filename의 값("main.js")으로 저장된다.(이때 name에 의해서 'assets/js/entry에 있는 이름.js'로 저장된다.)
