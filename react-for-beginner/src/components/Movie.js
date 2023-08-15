82b253f0228bfd34a220b4eaf871956920b3e90d
Cllaude99 <xodbsrla1025@naver.com>
2023-08-15 22:18:08 +0900

    END

--- react-for-beginner/src/components/Movie.js
+++ react-for-beginner/src/components/Movie.js
@@ -1,18 +1,22 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
-const Movie = ({ coverImg, title, summary, genres, id }) => (
-  <div>
-    <img src={coverImg} alt="title" />
-    <h2>
-      <Link to={`/movie/${id}`}>{title}</Link>
-    </h2>
-    <p>{summary}</p>
-    <ul>
-      {genres.map((g) => (
-        <li key={g}>{g}</li>
-      ))}
-    </ul>
+const Movie = ({ coverImg, title, year, summary, genres, id }) => (
+  <div className={styles.movie}>
+    <img src={coverImg} alt={title} className={styles.movie__img} />
+    <div>
+      <h2 className={styles.movie__title}>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
+      <h3 className={styles.movie__year}>{year}</h3>
+      <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+      <ul className={styles.movie__genres}>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
   </div>
 );
 
