1553c1da00d7aa821fa16ea8a6b2a7bfa6ca9911
TaeYoon Kim <xodbsrla1025@naver.com>
2023-08-15 19:02:45 +0900

    코드 정리

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -1,14 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Home, Detail } from "./routes";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movie/:id" element={<Detail />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movie/:id" element={<Detail />} />
+    </Routes>
   );
 };
 
