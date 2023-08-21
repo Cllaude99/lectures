dbbe03ea081ef6c0681f33fce3e5a5366f5431a1
Cllaude99 <xodbsrla1025@naver.com>
2023-08-21 13:51:03 +0900

    Feat: "as 와 attrs"

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,21 +1,25 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 const Father = styled.div`
   display: flex;
 `;
-const Box = styled.div`
-  width: 100px;
-  height: 100px;
-  background-color: ${(props) => props.bgColor};
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
-const Circle = styled(Box)`
-  border-radius: 50px;
+
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: tomato;
 `;
 function App() {
   return (
     <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
