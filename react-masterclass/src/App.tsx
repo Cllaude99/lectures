52d35b2932e6cd6ae5848259cf3f6e81b7555fff
Cllaude99 <xodbsrla1025@naver.com>
2023-08-22 19:58:21 +0900

    Feat: "Typescript에서 styled-components를 이용해 Theme 사용하기"

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -1,29 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setValue("");
-  };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          placeholder="username"
-          value={value}
-          onChange={onChange}
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>protected</H1>
+    </Container>
   );
 }
 
