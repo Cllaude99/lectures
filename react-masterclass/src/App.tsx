2689ce3098aebc4bab1a6267297d903f177817db
TaeYoon Kim <xodbsrla1025@naver.com>
2023-09-12 16:51:23 +0900

    #3

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -1,40 +1,28 @@
-import { keyframes, styled } from "styled-components";
-
-const Wrapper = styled.div`
-  display: flex;
-`;
-const rotationAnimation = keyframes`
-  from{
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  to{
-    transform: rotate(360deg);
-    border-radius: 50px;
-  }
-`;
-const Box = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: tomato;
-  width: 200px;
-  height: 200px;
-  animation: ${rotationAnimation} 1s linear infinite;
-  span {
-    &:hover {
-      font-size: 50px;
-    }
-  }
-`;
+import React, { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setUsername(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
   return (
-    <Wrapper>
-      <Box>
-        <span>😊</span>
-      </Box>
-    </Wrapper>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="username"
+          onChange={onChange}
+          value={username}
+        />
+      </form>
+    </div>
   );
 }
 
