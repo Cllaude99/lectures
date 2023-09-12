af0039ab746cc9dfaa82b48580889a6cd6d6a8f2
Cllaude99 <xodbsrla1025@naver.com>
2023-09-13 01:16:09 +0900

    #3 Recap

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -1,4 +1,12 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
   const [username, setUsername] = useState("");
