5fbe1ec534b3c91ecf5b05be929fe21f3a0206b4
Cllaude99 <xodbsrla1025@naver.com>
2023-10-02 01:00:33 +0900

    #8.4

--- framer-practice/src/App.tsx
+++ framer-practice/src/App.tsx
@@ -9,17 +9,51 @@ const Wrapper = styled(motion.div)`
   align-items: center;
 `;
 const Box = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  place-items: center center;
   width: 200px;
   height: 200px;
   border-radius: 40px;
-  background-color: rgba(255, 255, 255, 1);
+  background-color: rgba(255, 255, 255, 0.4);
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1) 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const Circle = styled(motion.div)`
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  background-color: rgba(255, 255, 255, 1);
+`;
+
 function App() {
+  const boxVariants = {
+    initial: { opacity: 0, scale: 0 },
+    animate: {
+      opacity: 1,
+      scale: 1,
+      transition: { type: "spring", delayChildren: 0.5, staggerChildren: 0.2 },
+    },
+  };
+  const circleVariants = {
+    initial: { y: 20, opacity: 0 },
+    animate: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+      },
+    },
+  };
+
   return (
     <Wrapper>
-      <Box />
+      <Box variants={boxVariants} initial="initial" animate="animate">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
