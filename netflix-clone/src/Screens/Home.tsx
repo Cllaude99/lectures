79e97d7b8466ab8a9a306846f004ae6ece2b8299
Cllaude99 <xodbsrla1025@naver.com>
2023-10-13 15:45:20 +0900

    #9.6

--- netflix-clone/src/Screens/Home.tsx
+++ netflix-clone/src/Screens/Home.tsx
@@ -1,9 +1,55 @@
 import { useQuery } from "react-query";
-import { getMovies } from "../api";
+import { IMoviesResult, getMovies } from "../api";
+import styled from "styled-components";
+import { makeImagePath } from "../utils";
 
+const Wrapper = styled.div`
+  background-color: black;
+`;
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Banner = styled.div<{ bgImage: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgImage});
+  background-size: cover;
+`;
+const Title = styled.h2`
+  font-size: 68px;
+  margin-bottom: 20px;
+`;
+const Overview = styled.p`
+  font-size: 36px;
+  width: 50%;
+`;
 const Home = () => {
-  const {} = useQuery([], getMovies);
-  return <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}></div>;
+  const { isLoading, data, error } = useQuery<IMoviesResult>(
+    ["movies"],
+    getMovies
+  );
+
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading</Loader>
+      ) : (
+        <>
+          <Banner bgImage={makeImagePath(data?.results[0].backdrop_path ?? "")}>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+        </>
+      )}
+    </Wrapper>
+  );
 };
 
 export default Home;
